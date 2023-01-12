acf7da689cdecdd248a8db256a3ab2abb692be9a
Sarathkumar-amat <poongodinall@gmail.com>
2023-01-12 07:21:18 +0530

    feat-added error handler

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var txt = document.querySelector("#txt-inp");
 var opVal=document.querySelector("#output");
 btnCheck.addEventListener("click",Handler)
 var ServerUrl = "https://api.funtranslations.com/translate/yoda.json";
+function errorHandler(error)
+{
+    console.log("error occured "+error);
+    alert("something is wrong with the server! Please try again later");
+}
 function Handler()
 {
     //opVal.innerText="translate "+txt.value;
@@ -11,7 +16,7 @@ function Handler()
     fetch(getTranslationUrl(textValue)).then(response=>response.json()).then(json=>{
         var Transtext=json.contents.translated;
         opVal.innerText=Transtext;
-    });
+    }).catch(errorHandler);
     
 };
 function getTranslationUrl(text)
